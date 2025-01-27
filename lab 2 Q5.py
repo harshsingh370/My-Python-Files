b7a1c3d95a410f2c8db9c3ada5f256910489e714
harshsingh370 <harshaman7422@gmail.com>
2025-01-27 21:05:55 +0530

    Add files via upload

---
+++ lab 2 Q5.py
@@ -0,0 +1,6 @@
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter the second number: "))
+if num1<num2:
+    print(f"The smallest number is {num1}")
+else:
+    print(f"The smallest number is {num2}")
