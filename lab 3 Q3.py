dd20d7db15949fc320ed90a856e2195e7ba8681a
harshsingh370 <harshaman7422@gmail.com>
2025-01-28 10:24:36 +0530

    Add files via upload

---
+++ lab 3 Q3.py
@@ -0,0 +1,4 @@
+#3. Write a python program to read two numbers and find the sum of their cubes
+num1 = int(input("Enter the first number : "))
+num2=  int(input("Enter the second number : "))
+print("The sum of cubes of num1 and num2 is :" ,num1 ** 3 + num2 ** 3) 
