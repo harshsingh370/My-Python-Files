3a3cac79af6cd9f18e56e668c341e05293bcf83f
harshsingh370 <harshaman7422@gmail.com>
2025-01-28 10:15:04 +0530

    Add files via upload

---
+++ lab 3 Q2.py
@@ -0,0 +1,9 @@
+num1 = int(input("Enter the first nuber: "))
+num2 = int(input("Enter the second number:  "))
+num1 = num1 + num2
+num2 = num1 -  num2
+num1 = num1 -  num2
+print("After swapping :")
+
+print("First number :", num1)
+print("second number: ", num2)
