8167ad8f46486ab3adfcb062c001d239ac10dd48
harshsingh370 <harshaman7422@gmail.com>
2025-01-28 10:41:57 +0530

    Update lab 3 Q4.py

--- lab 3 Q4.py
+++ lab 3 Q4.py
@@ -2,13 +2,16 @@
 num1 = int(input("Enter the first  number :  "))
 num2 = int(input("Enter the second number : "))
 num3  = int(input("Enter the third number : "))
-if num1 == num2:
+if num1 == num2 ==  num3:
+    print("All numbers are equal")
+elif  num1 == num2:
     print("Number 1 is equal to Number 2")
-elif  num1 == num3:
-    print("Number 1 is equal to Number 3")
+elif num1 == num3:
+    print("Number 1 is eual to Number 3")
 elif num2 == num3:
-    print("Number 2 is eual to Number 3")
+    print("Number 2 s equal to Number 3")
 else:
     print("All numbers are different")
 
+           
            
