dfe409ea02b039d214cf3a5e6b37a31a2933a47e
harshsingh370 <harshaman7422@gmail.com>
2025-01-28 10:33:47 +0530

    Add files via upload

---
+++ lab 3 Q4.py
@@ -0,0 +1,14 @@
+#4. Write a python program to read three numbers and if any two variables are equal, print that number
+num1 = int(input("Enter the first  number :  "))
+num2 = int(input("Enter the second number : "))
+num3  = int(input("Enter the third number : "))
+if num1 == num2:
+    print("Number 1 is equal to Number 2")
+elif  num1 == num3:
+    print("Number 1 is equal to Number 3")
+elif num2 == num3:
+    print("Number 2 is eual to Number 3")
+else:
+    print("All numbers are different")
+
+           
