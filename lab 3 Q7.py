9db5d407bab2685c1c869c79087dab8d3eec33d2
harshsingh370 <harshaman7422@gmail.com>
2025-02-03 21:13:12 +0530

    Add files via upload

---
+++ lab 3 Q7.py
@@ -0,0 +1,5 @@
+#Write a python program to read radius of a circle and print the area
+import math  
+radius = int(input("Enter the radius of the circle: "))
+area = math.pi * radius ** 2
+print("The area of the circle is:",area)
