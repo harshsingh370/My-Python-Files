0fddd0def61fdc8407c662af6f51d22ebd5d3f7d
harshsingh370 <harshaman7422@gmail.com>
2025-02-11 11:16:21 +0530

    Add files via upload

---
+++ lab 6 Q4.py
@@ -0,0 +1,5 @@
+# Write a python program to get the smallest number fron a list.
+numbers = [10,9,76,3,1]
+smallest_number = min(numbers)
+print("The smallest number in the list is:",smallest_number)
+
