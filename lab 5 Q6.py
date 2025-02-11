11bef4d151610d7b05bb0f463ebef9c1bc5dee63
harshsingh370 <harshaman7422@gmail.com>
2025-02-11 10:47:21 +0530

    Add files via upload

---
+++ lab 5 Q6.py
@@ -0,0 +1,10 @@
+# Write a Python program to count the number of characters (character frequency) in a string.
+my_string = input("Enter a string: ")
+char_count = {}
+for char in my_string:
+    if char in char_count:
+        char_count[char] += 1
+    else:
+        char_count[char] = 1
+
+print("Character frequency:", char_count)
