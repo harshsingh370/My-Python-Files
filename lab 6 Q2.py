b6feae7864beaf8bbe922c868018445f3466fb45
harshsingh370 <harshaman7422@gmail.com>
2025-02-11 11:09:00 +0530

    Add files via upload

---
+++ lab 6 Q2.py
@@ -0,0 +1,6 @@
+# Write a python program to multiplies all the items in a list.
+numbers = [1,2,3,4,5]
+result = 1
+for num in numbers:
+    result *= num
+    print("The product of all items in the list is:",result)
