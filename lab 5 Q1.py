033a4995aff4a1b2b67b3ebd67170e5c5572597b
harshsingh370 <harshaman7422@gmail.com>
2025-02-11 10:26:07 +0530

    Add files via upload

---
+++ lab 5 Q1.py
@@ -0,0 +1,8 @@
+# WAP to demonstrate while loop with else statement
+count = 0
+while count < 5:
+    print("Count is:", {count})
+    count += 1
+else:
+    print("The while loop has completed successfully.")
+
