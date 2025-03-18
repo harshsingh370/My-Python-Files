0e5599813b3d150ade36e3a966d5f41a1fc8a096
harshsingh370 <harshaman7422@gmail.com>
2025-03-18 10:03:31 +0530

    Add files via upload

---
+++ lab.8 check if 2 sets have any ele in common.py
@@ -0,0 +1,7 @@
+set1 = {10,20,30,40,50}
+set2 = {30,40,50,60,70}
+common_element = set1 & set2
+if common_element:
+    print("Common element:",common_element)
+else:
+    print("No common elements.")
